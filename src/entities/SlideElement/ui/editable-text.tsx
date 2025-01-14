e33d1936801ac08b1d2222afc2899b88b5f4f604
Dima Honcharenko <d.goncharenko.man@gmail.com>
2025-01-14 17:19:01 +0200

    Bug fixed: a previous element takes place of deleted one

--- src/entities/SlideElement/ui/editable-text.tsx
+++ src/entities/SlideElement/ui/editable-text.tsx
@@ -8,16 +8,24 @@ type Props = {
   element: SlideText
   onChange: (value: string) => void
   onDelete: () => void
+  onChangedPosition?: (newPosition: { x: number; y: number }) => void
 }
 
-export const EditableText = ({ element, onDelete, onChange }: Props) => {
+export const EditableText = ({
+  element,
+  onDelete,
+  onChange,
+  onChangedPosition,
+}: Props) => {
   return (
     <EditorWrapper onChange={onChange} content={element.content}>
       {editor => {
         return (
           <DraggableResizable
+            id={element.id}
             onDelete={onDelete}
             type='wyswyg'
+            onDragLeave={onChangedPosition}
             wyswygSlot={<WYSWYG content={element.content} editor={editor} />}
             initialPosition={element.position}
           />
--- src/features/AddNewSlide/ui/index.tsx
+++ src/features/AddNewSlide/ui/index.tsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
-import { placeCentered } from '@/widgets/Sidenav'
 import { Button } from '@/shared/ui/bricks/common/Button'
 import { AppStateContext } from '@/shared/context/app-state-context'
 import { SlideElementsContext } from '@/shared/context/slide-elements-context'
@@ -25,7 +24,19 @@ export const AddNewSlide = ({ currentSlideIndex }: Props) => {
             bg: 'red',
             type: 'text-node',
             content: 'New Text',
-            position: placeCentered(),
+            position: { x: 500, y: 300 },
+            size: {
+              width: 300,
+              height: 100,
+            },
+          },
+          {
+            'slide-id': `slide-${currentSlideIndex + 1}`,
+            id: `text-node-${uuidv4()}`,
+            bg: 'red',
+            type: 'text-node',
+            content: 'New Text',
+            position: { x: 500, y: 300 },
             size: {
               width: 300,
               height: 100,
