1e2a65a2bcdf27d356995ed176b9ce0046ef364f
Dima Honcharenko <d.goncharenko.man@gmail.com>
2025-01-18 17:59:19 +0200

    Added set color menu and color palettes

---
+++ src/shared/constants/local-storage-keys.ts
@@ -0,0 +1,3 @@
+export const STORAGE_KEYS = {
+  CUSTOM_PALETTE: 'CUSTOM_PALETTE',
+}
---
+++ src/widgets/Sidenav/lib/color-palettes.ts
@@ -0,0 +1,30 @@
+export const colors = {
+  DOOM: [
+    { color: '#202616', label: 'Desolate' },
+    { color: '#595945', label: 'Gloomy' },
+    { color: '#A6A485', label: 'Faded' },
+    { color: '#8C8A70', label: 'Ashen' },
+    { color: '#402E19', label: 'Charred' },
+  ],
+  VINTAGE: [
+    { color: '#BF1523', label: 'Timeless' },
+    { color: '#D9B504', label: 'Golden' },
+    { color: '#D9A407', label: 'Aged' },
+    { color: '#BCBDB8', label: 'Muted' },
+    { color: '#7D7D7F', label: 'Weathered' },
+  ],
+  ELEGANCY: [
+    { color: '#BFD1D9', label: 'Refined' },
+    { color: '#D8EBF2', label: 'Delicate' },
+    { color: '#D9B1AD', label: 'Graceful' },
+    { color: '#BF8E8E', label: 'Sophisticated' },
+    { color: '#F2D5D5', label: 'Soft' },
+  ],
+  CUSTOM: [
+    { color: 'black', label: 'Custom color 1' },
+    { color: 'white', label: 'Custom color 2' },
+    { color: 'green', label: 'Custom color 3' },
+    { color: 'yellow', label: 'Custom color 4' },
+    { color: 'orange', label: 'Custom color 5' },
+  ],
+}
