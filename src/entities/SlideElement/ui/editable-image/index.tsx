6d5f660703bd9e37fc3b4a4d01c419fb808784fb
Dima Honcharenko <d.goncharenko.man@gmail.com>
2025-02-04 03:03:36 +0200

    Fixed toggling of frames and filters of images

--- src/entities/SlideElement/ui/editable-image/index.tsx
+++ src/entities/SlideElement/ui/editable-image/index.tsx
@@ -2,26 +2,41 @@ import Image, { ImageProps } from 'next/image'
 
 import { cn } from '@/shared/lib/cn-merge'
 import { useImageAttributes } from '../../lib/useImageAttributes'
-import { parseFilterProperty, parseImageFrame } from '../../lib/utils'
+import {
+  parseFiltersAttributeToClassname,
+  parseFramesAttributeToClassname,
+} from '../../lib/utils'
 import { ImageContextMenu } from './context-menu'
 
 type Props = ImageProps
 
 export const EditableImage = ({ id, ...props }: Props) => {
-  const [attributes, setAttributes] = useImageAttributes()
+  const [
+    attributes,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _,
+    {
+      toggleFilterAttribute,
+      toggleFrameAttribute,
+      clearFiltersAttribute,
+      clearFramesAttribute,
+    },
+  ] = useImageAttributes()
 
   return (
     <ImageContextMenu
-      changeFilter={filter => setAttributes(prev => ({ ...prev, filter }))}
-      changeFrame={frame => setAttributes(prev => ({ ...prev, frame }))}
+      changeFilter={toggleFilterAttribute}
+      changeFrame={toggleFrameAttribute}
+      clearFrames={clearFramesAttribute}
+      clearFilters={clearFiltersAttribute}
       elementId={`${id}`}
     >
       <Image
         {...props}
         className={cn(
           '!m-0 !max-h-full !max-w-full select-none',
-          parseImageFrame(attributes.frame),
-          parseFilterProperty(attributes.filter),
+          parseFramesAttributeToClassname(attributes.frames),
+          parseFiltersAttributeToClassname(attributes.filters),
           props.className,
         )}
         fill
